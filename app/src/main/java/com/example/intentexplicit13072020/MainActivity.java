b7dbb79591f737284146a8afde4d2aa1cb25aa6e
phatdroid94 <phatdroid94@gmail.com>
2020-09-04 18:25:45 +0700

    arraylist

--- app/src/main/java/com/example/intentexplicit13072020/MainActivity.java
+++ app/src/main/java/com/example/intentexplicit13072020/MainActivity.java
@@ -8,21 +8,38 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button mBtnString;
+    Button mBtnString,mBtnArrayList;
+    ArrayList<String> mArrayListString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mBtnString = findViewById(R.id.buttonString);
+        mBtnArrayList = findViewById(R.id.buttonArrayList);
 
         mBtnString.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,MainActivity2.class);
                 intent.putExtra("chuoi","hello");
+                startActivity(intent);
+            }
+        });
+
+        mBtnArrayList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mArrayListString = new ArrayList<>();
+                mArrayListString.add("Tuan");
+                mArrayListString.add("Hoa");
+                mArrayListString.add("Ngoc");
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra("array",mArrayListString);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/intentexplicit13072020/MainActivity2.java
+++ app/src/main/java/com/example/intentexplicit13072020/MainActivity2.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
@@ -14,10 +16,16 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         // 1 : Nhan dang string
+//        Intent intent = getIntent();
+//        if (intent != null){
+//            String chuoi = intent.getStringExtra("chuoi");
+//            Log.d("BBB",chuoi);
+//        }
+        // 2 : Nhan dang array
         Intent intent = getIntent();
         if (intent != null){
-            String chuoi = intent.getStringExtra("chuoi");
-            Log.d("BBB",chuoi);
+            ArrayList<String> arrayString = intent.getStringArrayListExtra("array");
+            Log.d("BBB",arrayString.size() + "");
         }
     }
 }
